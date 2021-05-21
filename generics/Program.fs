7a9ba62a31980b66e24d1847c269107ef4027fe3
0918nobita <nobita.0918@gmail.com>
2021-05-22 08:55:10 +0900

    :sparkles: ジェネリック型引数を用いた関数のサンプルを追加した

--- generics/Program.fs
+++ generics/Program.fs
@@ -1,4 +1,11 @@
+/// リストを結合する
+let rec append xs ys =
+    match xs with
+    | [] -> ys
+    | x :: xs' -> x :: (append xs' ys)
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello world!"
+    printfn "%A" <| append [1; 2; 3] [4; 5; 6] // => [1; 2; 3; 4; 5; 6]
+    printfn "%A" <| append [true; false] [true] // => [true; false; true]
     0
